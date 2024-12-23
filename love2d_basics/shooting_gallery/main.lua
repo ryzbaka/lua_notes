46ea69e29f77dbfa6aeedbe7bbb2b6867a81f639
ryzbaka <hamzaali151214@hotmail.com>
2024-12-23 20:43:20 +0530

    basic shooter with sounds

--- love2d_basics/shooting_gallery/main.lua
+++ love2d_basics/shooting_gallery/main.lua
@@ -15,16 +15,19 @@ end
 
 function love.load()
     target = Target.new(love.graphics.getWidth(), love.graphics.getHeight())
+    beep = love.audio.newSource("sounds/beep.wav", "static")
 end
 
 function love.update()
     if (targetHit()) then
         target:reposition()
         target:scoreUp()
+        beep:play()
     end
 end
 
 function love.draw()
+    love.graphics.setColor(0, 100, 0)
     love.graphics.print(tostring(target.score), 100, 100)
     target:draw()
 end
