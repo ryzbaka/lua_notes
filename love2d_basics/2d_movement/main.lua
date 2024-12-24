3770e4558d00feb9ff3b043db17766769a7efa35
ryzbaka <hamzaali151214@hotmail.com>
2024-12-24 11:49:34 +0530

    singleton test and reposition sprite draw

--- love2d_basics/2d_movement/main.lua
+++ love2d_basics/2d_movement/main.lua
@@ -1,18 +1,28 @@
-Player = {}
+Player = {
+    count = 0
+}
 Player.__index = Player
 
 function Player.new()
+    if (Player.count == 1) then
+        --singleton
+        error("Cannot create more than 1 instance of player")
+    end
     local self = setmetatable({}, Player)
-    self.x = 0
-    self.y = 0
+    self.x = 0 + math.random(1, 20)
+    self.y = 0 + math.random(1, 20)
     self.radius = 20
     self.speed = 5
     self.sprite = love.graphics.newImage("sprites/p.png")
+    Player.count = Player.count + 1
     return self
 end
 
 function Player:draw()
-    love.graphics.draw(self.sprite, self.x, self.y, 0, 2, 2)
+    love.graphics.draw(self.sprite, self.x - self.radius, self.y - self.radius, 0, 2, 2)
+    -- love.graphics.setColor(1, 0, 0)
+    -- love.graphics.circle("fill", self.x, self.y, 10)
+    -- love.graphics.setColor(1, 1, 1)
 end
 
 function Player:moveRight()
@@ -33,6 +43,7 @@ end
 
 function love.load()
     player = Player.new()
+    -- player2 = Player.new() -- throws singleton error
 
     background_sprite = love.graphics.newImage("sprites/background.png")
 end
