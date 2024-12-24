f974e052032ecc83ff50c6a9cd6aaf6a79eebd3b
ryzbaka <hamzaali151214@hotmail.com>
2024-12-24 14:17:11 +0530

    updated player movement refactor

--- love2d_basics/anim_2d_movement/Entities/Player.lua
+++ love2d_basics/anim_2d_movement/Entities/Player.lua
@@ -14,28 +14,80 @@ function Player.new()
     --define animations
     self.idleAnimation = anim8.newAnimation(self.grid("1-10", 1), self.animationSpeed)
     self.rightAnimation = anim8.newAnimation(self.grid("1-10", 3), self.animationSpeed)
-    self.leftAnimation = anim8.newAnimation(self.grid("1-10", 3), self.animationSpeed):flipH()
+    self.leftAnimation = self.rightAnimation:clone():flipH()
+    self.idleAnimationLeft = self.idleAnimation:clone():flipH()
     -- anim holder
     self.anim = self.idleAnimation
+    -- movement state
+    self.isMoving = false
+    self.direction = "right"
     return self
 end
 
 function Player:draw()
-    self.anim:draw(self.spriteSheet, self.x, self.y, nil, 5, 5)
+    self.anim:draw(self.spriteSheet, self.x, self.y, nil, 5, 5) -- here scaling had to be set for both axes to prevent squishing
 end
 
 function Player:moveRight()
-    self.x = self.x + self.speed
-    self.anim = self.rightAnimation
+    self.isMoving = true            -- set state to moving
+    self.direction = "right"        -- set movement direction
+    self.x = self.x + self.speed    -- move player
+    self.anim = self.rightAnimation -- set movement animation
 end
 
 function Player:moveLeft()
+    self.isMoving = true
+    self.direction = "left"
     self.x = self.x - self.speed
     self.anim = self.leftAnimation
 end
 
+function Player:moveUp()
+    self.isMoving = true
+    self.y = self.y - self.speed
+    if self.direction == "right" then
+        self.anim = self.rightAnimation
+    else
+        self.anim = self.leftAnimation
+    end
+end
+
+function Player:moveDown()
+    self.isMoving = true
+    self.y = self.y + self.speed
+    if self.direction == "right" then
+        self.anim = self.rightAnimation
+    else
+        self.anim = self.leftAnimation
+    end
+end
+
+function Player:handleMovement(dt)
+    if love.keyboard.isDown("right") then
+        player:moveRight()
+    end
+    if love.keyboard.isDown("left") then
+        player:moveLeft()
+    end
+    if love.keyboard.isDown("up") then
+        player:moveUp()
+    end
+    if love.keyboard.isDown("down") then
+        player:moveDown()
+    end
+    self:updateAnimation(dt)
+end
+
 function Player:updateAnimation(dt)
+    if not self.isMoving then -- check if player is not moving this frame to set idle animation
+        if self.direction == "right" then
+            self.anim = self.idleAnimation
+        else -- player last moved left
+            self.anim = self.idleAnimationLeft
+        end
+    end
     self.anim:update(dt)
+    self.isMoving = false -- reset movement state tracker
 end
 
 return Player
--- love2d_basics/anim_2d_movement/main.lua
+++ love2d_basics/anim_2d_movement/main.lua
@@ -9,20 +9,9 @@ function love.load()
 end
 
 function love.update(dt)
-    local isMoving = false
-    if love.keyboard.isDown("right") then
-        player:moveRight()
-        isMoving = true
-    end
-    if love.keyboard.isDown("left") then
-        player:moveLeft()
-        isMoving = true
-    end
-
-    if not isMoving then
-        player.anim = player.idleAnimation
-    end
-    player:updateAnimation(dt)
+    -- move player and set animation
+    player:handleMovement(dt)
+    -- ---------------------------------
 end
 
 function love.draw()
