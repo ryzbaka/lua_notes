898f9f876a4ed98265157fe2b641318e0297139a
ryzbaka <hamzaali151214@hotmail.com>
2024-12-25 01:24:37 +0530

    repositioned some debug elements

--- love2d_basics/tilesetmap/utils/Debug.lua
+++ love2d_basics/tilesetmap/utils/Debug.lua
@@ -1,6 +1,6 @@
 function DebugConsole()
     if DEBUG then
-        love.graphics.print("X: " .. player.x, 0, 0)
+        love.graphics.print("X: " .. player.x, 0, 50)
         love.graphics.print("Y: " .. player.y, 0, 100)
         love.graphics.print("mouse X: " .. love.mouse.getX() .. " mouse Y: " .. love.mouse.getY(), 0, 150)
         love.graphics.print("cam X: " .. cam.x .. " cam Y: " .. cam.y, 0, 200)
