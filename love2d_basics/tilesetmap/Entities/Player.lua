b7da52d6c2fa2c1ebdc04068c215fce9e5a4cdd2
ryzbaka <hamzaali151214@hotmail.com>
2024-12-24 18:22:30 +0530

    camera movement with tiled map implementation

---
+++ love2d_basics/tilesetmap/Entities/Player.lua
@@ -0,0 +1,94 @@
+local anim8 = require("anim8")
+
+Player = {}
+Player.__index = Player
+
+function Player.new()
+    local self = setmetatable({}, Player)
+    self.x = 418
+    self.y = 314
+    self.speed = 2
+    self.animationSpeed = 0.1
+    self.spriteSheet = love.graphics.newImage("sprites/Player.png")
+    self.grid = anim8.newGrid(32, 32, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
+    --define animations
+    self.idleAnimation = anim8.newAnimation(self.grid("1-10", 1), self.animationSpeed)
+    self.rightAnimation = anim8.newAnimation(self.grid("1-10", 3), self.animationSpeed)
+    self.leftAnimation = self.rightAnimation:clone():flipH()
+    self.idleAnimationLeft = self.idleAnimation:clone():flipH()
+    -- anim holder
+    self.anim = self.idleAnimation
+    -- movement state
+    self.isMoving = false
+    self.direction = "right"
+    return self
+end
+
+function Player:draw()
+    self.anim:draw(self.spriteSheet, self.x, self.y, nil, 4, 4, 16, 16) -- here scaling had to be set for both axes to prevent squishing
+    -- love.graphics.circle("fill", self.x, self.y, 20)
+end
+
+function Player:moveRight()
+    self.isMoving = true            -- set state to moving
+    self.direction = "right"        -- set movement direction
+    self.x = self.x + self.speed    -- move player
+    self.anim = self.rightAnimation -- set movement animation
+end
+
+function Player:moveLeft()
+    self.isMoving = true
+    self.direction = "left"
+    self.x = self.x - self.speed
+    self.anim = self.leftAnimation
+end
+
+function Player:moveUp()
+    self.isMoving = true
+    self.y = self.y - self.speed
+    if self.direction == "right" then
+        self.anim = self.rightAnimation
+    else
+        self.anim = self.leftAnimation
+    end
+end
+
+function Player:moveDown()
+    self.isMoving = true
+    self.y = self.y + self.speed
+    if self.direction == "right" then
+        self.anim = self.rightAnimation
+    else
+        self.anim = self.leftAnimation
+    end
+end
+
+function Player:handleMovement(dt)
+    if love.keyboard.isDown("right") then
+        player:moveRight()
+    end
+    if love.keyboard.isDown("left") then
+        player:moveLeft()
+    end
+    if love.keyboard.isDown("up") then
+        player:moveUp()
+    end
+    if love.keyboard.isDown("down") then
+        player:moveDown()
+    end
+    self:updateAnimation(dt)
+end
+
+function Player:updateAnimation(dt)
+    if not self.isMoving then -- check if player is not moving this frame to set idle animation
+        if self.direction == "right" then
+            self.anim = self.idleAnimation
+        else -- player last moved left
+            self.anim = self.idleAnimationLeft
+        end
+    end
+    self.anim:update(dt)
+    self.isMoving = false -- reset movement state tracker
+end
+
+return Player
--- love2d_basics/tilesetmap/main.lua
+++ love2d_basics/tilesetmap/main.lua
@@ -1,15 +1,42 @@
 package.path = package.path .. ";../libraries/?.lua;../libraries/?/init.lua"
 
+Player = require("Entities.Player")
 
 function love.load()
-    anim8 = require("anim8") -- Access ../libraries/anim8.lua
-    sti = require("sti")     -- Access ../libraries/sti/init.lua
+    -- anim8 = require("anim8") -- Access ../libraries/anim8.lua
+    camera = require("camera")
+    cam = camera()
+
+    sti = require("sti") -- Access ../libraries/sti/init.lua
     gameMap = sti("maps/test_forest.lua")
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    player = Player.new()
+end
+
+function love.update(dt)
+    player:handleMovement(dt)
+    cam:lookAt(player.x, player.y)
 end
 
 function love.draw()
-    love.graphics.push()       -- Save the current coordinate system
-    love.graphics.scale(20, 2) -- Scale the map by 2x (adjust as needed)
-    gameMap:draw(0, 0, 3, 3)   -- Draw the map
+    -- this is code for drawing stuff without the camera
+    -- gameMap:draw(0, 0, 5, 5) -- Draw the map
+    -- player:draw()
+
+
+
+    cam:attach()
+    love.graphics.push()
+    love.graphics.scale(4, 4)
+    gameMap:drawLayer(gameMap.layers["grass"])
+    gameMap:drawLayer(gameMap.layers["Tile Layer 2"])
+    gameMap:drawLayer(gameMap.layers["Tile Layer 3"])
     love.graphics.pop()
+    player:draw()
+
+    cam:detach()
+
+    --DEBUG
+    love.graphics.print("X: " .. player.x, 0, 0)
+    love.graphics.print("Y: " .. player.y, 0, 100)
 end
